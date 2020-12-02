229a1552f92fefb7fbeb9474352e821f7a7e7d3d
Przemyslaw Skrzypczynski <przemekskrzypczynski4@wp.pl>
2020-12-02 10:54:47 +0100

    add empty TopNavigation component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import TopNavigation from './Components/TopNavigation';
 
 
 class App extends React.Component {
@@ -8,18 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hello
-          </a>
+          <TopNavigation />
         </header>
       </div>
     );
---
+++ src/Components/TopNavigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class TopNavigation extends React.Component {
+  render () {
+    return (
+        <nav>
+          <ul className="TopNav">
+              <li>1</li>
+              <li>2</li>
+          </ul>
+        </nav>
+    );
+  }
+}
+
+export default TopNavigation;
